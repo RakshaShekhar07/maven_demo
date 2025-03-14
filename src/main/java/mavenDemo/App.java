2858b718f27e6fbb2b31897a77fa96627a03cc8e
RakshaShekhar07 <154491318+RakshaShekhar07@users.noreply.github.com>
2025-03-14 18:37:01 +0530

    Update App.java

--- src/main/java/mavenDemo/App.java
+++ src/main/java/mavenDemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is Raksha" );
+        System.out.println( "Hello Guys!! Good evening" );
     }
 }
